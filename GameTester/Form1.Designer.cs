9e9bcf43649180e6e3a73a9030c00a0c14fcb7b9
gr99182 <gr99182@LR009MMC>
2019-11-05 13:56:08 -0600

    Add project files.

---
+++ GameTester/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace GameTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSetDefault = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblForDynamicItems = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(73, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 100);
+            this.label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(263, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(424, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 100);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnSetDefault
+            // 
+            this.btnSetDefault.Location = new System.Drawing.Point(778, 78);
+            this.btnSetDefault.Name = "btnSetDefault";
+            this.btnSetDefault.Size = new System.Drawing.Size(75, 23);
+            this.btnSetDefault.TabIndex = 3;
+            this.btnSetDefault.Text = "Set Default";
+            this.btnSetDefault.UseVisualStyleBackColor = true;
+            this.btnSetDefault.Click += new System.EventHandler(this.btnSetDefault_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "These were built draging from the toolbox";
+            // 
+            // lblForDynamicItems
+            // 
+            this.lblForDynamicItems.AutoSize = true;
+            this.lblForDynamicItems.Location = new System.Drawing.Point(96, 318);
+            this.lblForDynamicItems.Name = "lblForDynamicItems";
+            this.lblForDynamicItems.Size = new System.Drawing.Size(0, 13);
+            this.lblForDynamicItems.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 607);
+            this.Controls.Add(this.lblForDynamicItems);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSetDefault);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSetDefault;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblForDynamicItems;
+    }
+}
+
---
+++ GameTester/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        PictureBox aPictureBox2 = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // add text to the nonvisible label
+            lblForDynamicItems.Text = "These were built during runtime";
+            //Make a textbox
+            TextBox aTextBox = new TextBox();               // instantiate a textbox
+            aTextBox.Multiline = true;                      // allow it to grow
+            aTextBox.BackColor = Color.Blue;                // set the background color
+            aTextBox.BorderStyle = BorderStyle.FixedSingle; // put a boarder on it
+            aTextBox.Size = new Size(100, 100);             // set the size
+            aTextBox.Location = new Point(10, 400);         // set the location
+            Controls.Add(aTextBox);                         // add it to the form
+
+            // Make a pictureBox
+            PictureBox aPictureBox = new PictureBox();      // instantiate a picturebox
+            aPictureBox.Size = new Size(100, 100);          // set the size
+            aPictureBox.BackColor = Color.Blue;             // set the background color
+            aPictureBox.BorderStyle = BorderStyle.FixedSingle; //put a border on it
+            aPictureBox.Location = new Point(230, 400);     // set the location
+            Controls.Add(aPictureBox);                      // add it to the form
+
+
+            // Make a Label
+            Label aLabel = new Label();                     // instantiate a label
+            aLabel.AutoSize = false;                        // turn off autosize for a fixed size
+            aLabel.BorderStyle = BorderStyle.FixedSingle;   // put a boarder on it
+            aLabel.Size = new Size(100, 100);               // set the size
+            aLabel.BackColor = Color.Blue;                  // set the background color
+            aLabel.Location = new Point(120, 400);          // set the location
+            Controls.Add(aLabel);                           // add it to the form
+
+            // Make a pictureBox#2
+            aPictureBox2 = new PictureBox                   // an alternate way to instantiate a Picture box
+            {
+                Size = new Size(100, 100),
+                BackColor = Color.BlueViolet,
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(350, 400)
+            };
+            Controls.Add(aPictureBox2);
+            
+
+
+
+
+        }
+
+        private void btnSetDefault_Click(object sender, EventArgs e)
+        {
+            aPictureBox2.BackColor = DefaultBackColor;
+            
+        }
+    }
+}
